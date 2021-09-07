b7c07ff5a4c00f63af9fbd8a8b6255ee904e4d3c
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-09-07 20:51:22 +0530

    Added: Project files

---
+++ src/main/java/com/io/EmployeePayrollService.java
@@ -0,0 +1,13 @@
+package com.io;
+
+/**
+ * Employee Payroll Service System
+ *
+ */
+public class EmployeePayrollService
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Employee Payroll Service System!" );
+    }
+}
